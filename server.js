7cce31443dff04b7045119534034ddce9ccf6d0d
Dominik Widomski <dominik@digital-detox.co.uk>
2018-01-07 23:36:23 +0000

    feat(slack-server): add simple slack action response

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const { createMessageAdapter } = require('@slack/interactive-messages');
+
+// Initialize adapter using verification token from environment variables
+const slackMessages = createMessageAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+
+// Attach action handlers by `callback_id`
+// (See: https://api.slack.com/docs/interactive-message-field-guide#attachment_fields)
+slackMessages.action('wopr_game', (payload) => {
+  // `payload` is JSON that describes an interaction with a message.
+  console.log(`The user ${payload.user.name} in team ${payload.team.domain} pressed the welcome button`);
+
+  // The `actions` array contains details about the specific action (button press, menu selection, etc.)
+  const action = payload.actions[0];
+  console.log(`The button had name ${action.name} and value ${action.value}`);
+
+  // You should return a JSON object which describes a message to replace the original.
+  // Note that the payload contains a copy of the original message (`payload.original_message`).
+  const replacement = payload.original_message;
+  // Typically, you want to acknowledge the action and remove the interactive elements from the message
+  replacement.text =`Welcome ${payload.user.name}`;
+  delete replacement.attachments[0].actions;
+  return replacement;
+});
+
+// Start the built-in HTTP server
+const port = process.env.PORT || 3000;
+slackMessages.start(port).then(() => {
+  console.log(`server listening on port ${port} at /slack/actions`);
+});
