9e6ac44b3d36224ac6420203a93976b5d0958294
Dominik Widomski <dominik@digital-detox.co.uk>
2018-02-11 16:27:33 +0000

    feat(justin-cli): setup babel build and flow

---
+++ taskfile.js
@@ -0,0 +1,21 @@
+const { join } = require('path');
+
+const paths = {
+    build: 'build',
+    source: 'src/**/*.js',
+    sourceRoot: join(__dirname, 'src'),
+};
+
+module.exports = {
+    *default(task) {
+        yield task.start('scripts');
+        yield task.watch(['index.js', paths.source], 'scripts');
+    },
+    *clear(task) {
+        yield task.clear(paths.build);
+    },
+    *scripts(task) {
+        yield task.source(paths.source).babel().target(`${paths.build}/src`);
+        yield task.source(paths.source).babel().target(`${paths.build}/src`);
+    }
+}
