77cd42e0a8a5e70e06cb527b9a6bfe0e9462c5ff
Dominik Widomski <dominik@digital-detox.co.uk>
2018-02-27 18:47:28 +0100

    wip on build setup - still not ideal
    Would have liked to have source maps working correctly through the taskr babel,
    but maybe there's actually an issue with how it's implemented.
    Or, I don't get how it's suppose to be used and work.

--- taskfile.js
+++ taskfile.js
@@ -2,20 +2,21 @@ const { join } = require('path');
 
 const paths = {
     build: 'build',
-    source: 'src/**/*.js',
+    source: 'src',
+    sourceWatch: 'src/**/*.js',
     sourceRoot: join(__dirname, 'src'),
 };
 
 module.exports = {
     *default(task) {
         yield task.start('scripts');
-        yield task.watch(paths.source, 'scripts');
+        yield task.watch(paths.sourceWatch, 'scripts');
     },
     *clear(task) {
         yield task.clear(paths.build);
     },
     *scripts(task) {
         // yield task.source(paths.source).babel().target(`${paths.build}/src`);
-        yield task.source(paths.source).babel().target(`${paths.build}`);
+        yield task.source(paths.sourceWatch).babel().target(paths.build);
     }
 }
