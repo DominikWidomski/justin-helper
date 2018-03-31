cc8497695c82d82d5ffd8fc07e8f33ebdf463621
Dominik Widomski <dominik@digital-detox.co.uk>
2018-04-01 00:46:05 +0100

    fix(justin-cli): misc fixes and types to showWeekTable

--- src/actions/showWeekTable.js
+++ src/actions/showWeekTable.js
@@ -3,7 +3,7 @@
 const Table = require('cli-table2');
 const DateUtil = require("../utils/date");
 
-import type { ProjectTime } from "../types.js";
+import type { ProjectTime, Project, JustinResponse } from "../types.js";
 
 type showWeekTableOptionsType = {
 	startDate: string | Date,
@@ -33,18 +33,24 @@ const showWeekTable = (
 	options.endDate = new Date(options.endDate);
 	
 	// Sort chronologically
-	projectTimes
-		.sort((a, b) => new Date(a.attributes.date) > new Date(b.attributes.date))
+	projectTimes 
+		.sort((a, b) => new Date(a.attributes.date) - new Date(b.attributes.date))
 		.filter(time => {
 			const date = new Date(time.attributes.date);
-			return date >= options.startDate && date <= options.endDate;
+			return new Date(date) >= new Date(options.startDate) && new Date(date) <= new Date(options.endDate);
 		})
 		.forEach((projectTime, index, arr) => {
 			const { date, duration_mins, project_id } = projectTime.attributes;
-			const projectName = projects.data.find(project => project.id === project_id).attributes.name;
+			const project = projects.data.find(project => project.id === project_id);
+			const projectName = project ? project.attributes.name : 'n/a';
 			const firstOfProject = index === 0 || arr[index].attributes.date !== arr[index - 1].attributes.date;
 
-			table.push([ firstOfProject ? DateUtil.getNameOfDay(date) : '', date, projectName, duration_mins / 60 ]);
+			table.push([
+				firstOfProject ? DateUtil.getNameOfDay(date) : '',
+				firstOfProject ? date : '',
+				projectName,
+				duration_mins / 60
+			]);
 		});
 
 	console.log(table.toString());
