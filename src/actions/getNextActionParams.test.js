790c9f310327876779809dfe1cd99bec6d586d3b
Dominik Widomski <dominik@digital-detox.co.uk>
2018-04-07 18:02:27 +0100

    wip(justin-cli): stdin mocking for inquirer prompt testing
    Kind of got the mocking in the right order I expect, with a _simple_ test case.
    Have to cleanup, extract into a test utility, test with the full action.
    Have to also check with the console output / stdout capturing util that the prompt works with that capturing output (should do).
    And silence the output when we want.

--- src/actions/getNextActionParams.test.js
+++ src/actions/getNextActionParams.test.js
@@ -3,6 +3,24 @@ const mockStdin = require('mock-stdin');
 const inquirer = require('inquirer');
 const getNextActionParams = require("./getNextActionParams");
 
+if (!('describe' in global)) {
+    global.describe = (description, fn) => fn();
+    global.test = (description, fn) => fn();
+    global.beforeEach = () => {};
+    global.afterEach = () => {};
+    global.expect = () => {};
+}
+
+// Thanks https://github.com/bahmutov/bdd-stdin/blob/master/index.js
+const keys = Object.freeze({
+    up: '\u001b[A',
+    down: '\u001b[B',
+    left: '\u001b[D',
+    right: '\u001b[C',
+    enter: '\n',
+    space: ' '
+});
+
 // Originally found the https://github.com/neoziro/std-mocks/
 // but wasn't really doing it for me, was easier to write new one
 function mockStdoutWrite() {
@@ -93,47 +111,83 @@ describe("actions", () => {
             //         }
             //     }
             // });
-
             const stdin = mockStdin.stdin();
-            process.nextTick(function mockResponse() {
-                stdin.send('\u001b[B');
-                stdin.send('\n');
-            });
-
-            var question = {
-                type: 'list',
-                name: 'choice',
-                message: 'pick three',
-                choices: ['one', 'two', 'three']
+            const wait = time => {
+                return new Promise(resolve => setTimeout(resolve, time));
+            }
+            const waitTicks = (ticks, waitTime = 0) => {
+                if (ticks > 0) {
+                    process.nextTick(async () => {
+                        console.log('Waiting', ticks, waitTime);
+                        if (waitTime > 0) await wait(waitTime);
+                        waitTicks(--ticks, waitTime);
+                    })
+                }
             };
+
+            function mockInput() {
+                process.nextTick(async function mockResponse() {
+                    // await wait(1000);
+                    stdin.send(keys.down);
+                    stdin.send(keys.space);
+                    // await wait(1000);
+                    stdin.send(keys.down);
+                    // await wait(1000);
+                    stdin.send(keys.enter);
+                });
+            }
+
+            // mockInput();
+
             const _prompt = inquirer.prompt;
             // problem with the 1st inquirer.prompt question in this case is that it has a dynamic output, callback
             // so still have to call real prompt to trigger that functionality
-            inquirer.prompt = function (questions, cb) {
-                console.log('question');
-                if (questions.length === 1 &&
-                  questions[0].name === 'all') {
-                  setTimeout(function () {
-                    cb({
-                      all: ['one']
-                    });
-                  }, 0);
-                  return;
-                }
-          
-                setTimeout(function () {
-                  _prompt(question, cb);
-                }, 0);
+            inquirer.prompt = function (questions) {
+                // if (false && questions.length === 1 && questions[0].name === 'all') {
+                //     console.log('GONNA RESOLVE WITH ANSWER "one"');
+
+                //     return new Promise(resolve => {
+                //         setTimeout(function () {
+                //             resolve({
+                //                 [questions[0].name]: ['one']
+                //             });
+                //         }, 0);
+                //     });
+
+                //     return Promise.reject();
+                // }
+
+                // this is now just a pointless empty proxy
+                return new Promise(resolve => { 
+                    setTimeout(async function() {
+                        mockInput();
+                        // process.nextTick(function mockResponse() {
+                            // stdin.send('\u001b[B'); // down arrow
+                            // stdin.send('\n');
+                            // waitTicks(3, 2000);
+                        // });
+                        
+                        const ans = await _prompt(questions);
+                        resolve(ans);
+                    }, 0);
+                });
             };
             
-            inquirer.prompt([{
+            const questionCheckbox = {
                 type: 'checkbox',
                 name: 'all',
                 message: 'pick options',
                 choices: ['one', 'two', 'three']
-            }], function (answers) {
-                console.log('user picked', answers.choice);
-            });
+            };
+            const questionList = {
+                type: 'list',
+                name: 'choice',
+                message: 'pick third option',
+                choices: ['one', 'two', 'three']
+            };
+            const ans = await inquirer.prompt([questionList]);
+
+            console.log('ANSWER:', ans);
             
             let prompts = [];
             let output
@@ -159,7 +213,9 @@ describe("actions", () => {
 
             // expect(consoleLogBuffer).toMatchSnapshot();
             // expect(prompts).toMatchSnapshot();
-            expect(mock.flush()).toMatchSnapshot();
+            if (mock && mock.flush) { // temp
+                expect(mock.flush()).toMatchSnapshot();
+            }
         })
     })
 })
