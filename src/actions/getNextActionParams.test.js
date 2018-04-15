0652be049a61e84e3ba495fa5c9de2ad1b3641cf
Dominik Widomski <dominik@digital-detox.co.uk>
2018-04-15 15:34:14 +0100

    chore(justin-cli): bit of a cleanup to previous commit (fixup)

--- src/actions/getNextActionParams.test.js
+++ src/actions/getNextActionParams.test.js
@@ -71,61 +71,40 @@ describe("actions", () => {
          * (whichever way) to have it run automatically, the user might not see output on screen from specific actions etc.
          */
         test("default", async () => {
-            let prompts = [];
-            let output;
-            // prompt at the moment prevents stdout mock from capturing the output...
-            // kinda would like both.
-            // const originalPrompt = inquirer.prompt;
-            // inquirer.prompt = function(...args) {
-            //     prompts.push(args);
-            //     // next the user chooses from available options
-            //     // but something else would handle the output
-            //     originalPrompt(...args);
-            // };
-            // const consoleLogBuffer = bufferConsoleLog(async () => {
+            process.nextTick(async function mockResponse() {
+                stdin.send(keys.up);
+                // mock.flush() // checkpoint, next flush will be from here
+                stdin.send(keys.up);
+                // expect(mock.flush()).toMatchSnapshot(); // before 'ENTER', question one final state
+                stdin.send(keys.enter);
+                
                 process.nextTick(async function mockResponse() {
-                    stdin.send(keys.up);
-                    // mock.flush() // checkpoint, next flush will be from here
-                    stdin.send(keys.up);
-                    // expect(mock.flush()).toMatchSnapshot(); // before 'ENTER', question one final state
-                    stdin.send(keys.enter);
+                    // await wait(0); // this alows the thing to update, but not quite, sometimes it's not timed right                     
+
+                    // TODO: inquirer autocomplete has source: async callback, gotta wait for it to update
+                    // but there is probably a more correct way of doing that.
+                    // i.e. wait for a promise to resolve somewhere to have the certainty
+                    // TODO: Also, silence inquirer output
                     
-                    process.nextTick(async function mockResponse() {
-                        // await wait(0); // this alows the thing to update, but not quite, sometimes it's not timed right                     
+                    // this is to try to capture only the settle down output of the question
+                    // question one response
+                    // expect(mock.flush()).toMatchSnapshot();
+                    
+                    await wait(100); // TODO: It's like I need to wait here for inquirer to update (async sources?)
+                    stdin.send('private'); // TODO: ignore all the output while typing (a lot) and snapshot directly after input, and after confirming
+                    await wait(100); // TODO: It's like I need to wait here for inquirer to update (async sources?)
+                    stdin.send(keys.enter);
 
-                        // TODO: inquirer autocomplete has source: async callback, gotta wait for it to update
-                        // but there is probably a more correct way of doing that.
-                        // i.e. wait for a promise to resolve somewhere to have the certainty
-                        // TODO: Also, silence inquirer output
-                        
-                        // this is to try to capture only the settle down output of the question
-                        // question one response
-                        // expect(mock.flush()).toMatchSnapshot();
-                        
-                        await wait(100); // TODO: It's like I need to wait here for inquirer to update (async sources?)
-                        stdin.send('private'); // TODO: ignore all the output while typing (a lot) and snapshot directly after input, and after confirming
-                        await wait(100); // TODO: It's like I need to wait here for inquirer to update (async sources?)
-                        stdin.send(keys.enter);
-
-                        // TODO: Had a thought. Maybe it's cool to capture console output manually at certan points.
-                        // literally do like buffer += stdOutMock.flush() or something.
-                        // this is to capture the console output at certain points when we're expecting changes,
-                        // not the whole thing while it's in flux.
-                        // This would be alongside the general catch-all.
-                    });
+                    // TODO: Had a thought. Maybe it's cool to capture console output manually at certan points.
+                    // literally do like buffer += stdOutMock.flush() or something.
+                    // this is to capture the console output at certain points when we're expecting changes,
+                    // not the whole thing while it's in flux.
+                    // This would be alongside the general catch-all.
                 });
-                const answer = await getNextActionParams(projectTime, projects);
-            // });
+            });
+            const answer = await getNextActionParams(projectTime, projects);
 
-            // expect(consoleLogBuffer).toMatchSnapshot();
-            // expect(prompts).toMatchSnapshot();
-            // if (mock && mock.flush) { // temp
-            //     expect(mock.flush()).toMatchSnapshot();
-            // }
-
-            if (mock && mock.flush) {
-                expect(mock.flush()).toMatchSnapshot();
-            }
+            expect(mock.flush()).toMatchSnapshot();
             expect(answer).toMatchSnapshot();
         })
     })
