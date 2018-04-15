2d06e38fd7073d6c848b4a717b7fad9c63458263
Dominik Widomski <dominik@digital-detox.co.uk>
2018-04-15 15:28:05 +0100

    test(justin-cli): add stdout capture mock utility

---
+++ src/actions/utils/mockStdout.js
@@ -0,0 +1,36 @@
+// Originally found the https://github.com/neoziro/std-mocks/
+// but wasn't really doing it for me, was easier to write new one
+// I'm curious to Proxy the stdin to see how it clears the output to hook into that maybe
+module.exports = function mockStdoutWrite() {
+    const config = {
+        stripColors: true
+    };
+    
+    let lastFlushIndex = 0;
+    const originalStdoutWrite = process.stdout.write;
+    const buffer = [];
+
+    process.stdout.write = function (data) {
+        if ((typeof data === "string") && config.stripColors) {
+            const exp = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
+            data = data.replace(exp, '');
+        }
+        
+        buffer.push(data);
+    }
+
+    return {
+        restore: () => {
+            process.stdout.write = originalStdoutWrite;
+        },
+        // bypasses the capturing, useful for debugging
+        debug: (...args) => originalStdoutWrite(...args),
+        flushAll: () => buffer,
+        flush: () => {
+            const flushIndex = lastFlushIndex || 0;
+            lastFlushIndex = buffer.length;
+
+            return buffer.slice(flushIndex);
+        }
+    }
+}
