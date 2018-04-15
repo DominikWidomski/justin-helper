bde0b1ab898b655adb7b1ebf4d1203860f525927
Dominik Widomski <dominik@digital-detox.co.uk>
2018-04-15 15:27:38 +0100

    test(justin-cli): add keys consts for stdin input

---
+++ src/actions/utils/keys.js
@@ -0,0 +1,9 @@
+// Thanks https://github.com/bahmutov/bdd-stdin/blob/master/index.js
+module.exports = Object.freeze({
+    up: '\u001b[A',
+    down: '\u001b[B',
+    left: '\u001b[D',
+    right: '\u001b[C',
+    enter: '\n',
+    space: ' '
+});
